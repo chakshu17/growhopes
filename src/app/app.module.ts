c6ad5f30b4c59aabb113bffb377eec704274662b
Chakshu kishnani <chakshu05kishnani@gmail.com>
2020-08-10 14:00:19 +0530

    update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,11 +9,13 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { OurservicesComponent } from './ourservices/ourservices.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    OurservicesComponent
+    OurservicesComponent,
+    HomeComponent,
   ],
   imports: [
     BrowserModule,
